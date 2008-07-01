4927392e26e0a35e0bae0c2735c6edc732edd7f5
hito <hito>
2008-07-01 09:59:56 +0000

    *** empty log message ***

---
+++ src/gtk/ogra2cairofile.h
@@ -0,0 +1,18 @@
+#ifndef _O_GRA2CAIROFILE_HEADER
+#define _O_GRA2CAIROFILE_HEADER
+
+
+
+enum surface_type_id {
+  TYPE_PS2,
+  TYPE_PS3,
+  TYPE_EPS2,
+  TYPE_EPS3,
+  TYPE_PDF,
+  TYPE_SVG1_1,
+  TYPE_SVG1_2,
+  TYPE_PNG,
+};
+
+#endif
+
