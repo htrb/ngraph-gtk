ae542b45573f96225aee7e02fd52c066d842b4ef
hito <hito>
2009-11-24 05:28:49 +0000

    *** empty log message ***

---
+++ test/math_test.rb
@@ -0,0 +1,16 @@
+#! /usr/bin/ruby
+puts("set +e")
+
+while (l = gets)
+  a = l.chomp.split("\t")
+  print <<EOF
+RESULT=`dexpr '#{a[0]}'`
+if [ "$RESULT" != "#{a[1]}" ]
+then
+  echo error '#{a[0]}' '#{a[1]}' "$RESULT".
+fi
+
+EOF
+end
+
+puts("del system")
