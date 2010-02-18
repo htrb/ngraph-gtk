0e98e251a6391a135eca870851b055f1a77ae3cd
hito <hito>
2010-02-18 08:15:35 +0000

    *** empty log message ***

--- pathconv.sh
+++ pathconv.sh
@@ -1,23 +1,21 @@
 #! /bin/sh
 
-DOCDIR=`echo $1 | sed 's/\//\\\\\//g'`
-
+DOCDIR=$1
 shift
 
-LIBDIR=`echo $1 | sed 's/\//\\\\\//g'`
-
+LIBDIR=$1
 shift
 
 if [ "X$2" != "X" ]
 then
-    CONFDIR=`echo $1 | sed 's/\//\\\\\//g'`
+    CONFDIR=$1
     shift
 fi
 
 TARGET=`basename $1 .in`
 
-cat $1 | sed -e s/DOCDIRDEF/$DOCDIR/g \
-             -e s/LIBDIRDEF/$LIBDIR/g \
-             -e s/CONFDIRDEF/$CONFDIR/g > $TARGET
+cat $1 | sed -e s!DOCDIRDEF!$DOCDIR!g \
+             -e s!LIBDIRDEF!$LIBDIR!g \
+             -e s!CONFDIRDEF!$CONFDIR!g > $TARGET
 
 echo $CONFDIR
