327090cd7b5ff12c5b28e80044a850a2f41075a0
htrb <htrb@a1697baf-5ae0-492e-bcf4-638f3baa2ba1>
2012-02-13 09:34:26 +0000

    * mingw/associate.bat: change line feed code.
    * mingw/echo.nsc: show program neme and version.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/ngraph-gtk/code/trunk@417 a1697baf-5ae0-492e-bcf4-638f3baa2ba1

--- mingw/associate.bat
+++ mingw/associate.bat
@@ -1,4 +1,4 @@
 @echo off
 ASSOC .ngp=NgraphGraph
 FTYPE NgraphGraph="%CD%\bin\ngraph.exe" "%%1" %%*
-bin\ngraph.exe -i echo.nsc "Ngraph-gtk: Instalation is finished."
+bin\ngraph.exe -i echo.nsc "Instalation is finished."
