00d928d3551b4193ea0d78773b9d735307c91ce7
Hiroyuki Ito <ZXB01226@nifty.com>
2016-12-20 18:49:07 +0900

    update mingw package support.

--- mingw/windows64.sh
+++ mingw/windows64.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-PKG_DIR=/`echo $HOMEDRIVE|sed -e's/://'`/ngraph-gtk64
-WIN_PATH=/mingw64
+PKG_DIR=ngraph-gtk
+WIN_PATH=/mingw
 
 ARCHIVE=`ls ngraph-gtk-*.tar.gz | tail -1`
 if [ -z "$ARCHIVE" ]
@@ -10,63 +10,88 @@ then
     exit
 fi
 
-makepkg-mingw -fs
-
 VERSION=`basename $ARCHIVE '.tar.gz' | sed -e 's/ngraph-gtk-//'`
-PKGFILE=mingw-w64-x86_64-ngraph-gtk-${VERSION}-1-any.pkg.tar.xz
-if [ ! -f "$PKGFILE" ]
-then
-    echo "cannot find the package file."
-    exit
-fi
-
-pacman -U $PKGFILE
 
 BINFILES="libatk-1.0-0.dll libbz2-1.dll libcairo-2.dll
 libcairo-gobject-2.dll libepoxy-0.dll libexpat-1.dll libffi-6.dll
-libfontconfig-1.dll libfreetype-6.dll libgcc_s_seh-1.dll
-libgdk_pixbuf-2.0-0.dll libgdk-3-0.dll libgio-2.0-0.dll
-libglib-2.0-0.dll libgmodule-2.0-0.dll libgobject-2.0-0.dll
-libgraphite2.dll libgsl-19.dll libgslcblas-0.dll libgtk-3-0.dll
-libgtksourceview-3.0-1.dll libharfbuzz-0.dll libiconv-2.dll
-libintl-8.dll libobjc-4.dll libp11-kit-0.dll libpango-1.0-0.dll
+libfontconfig-1.dll libfreetype-6.dll libgdk_pixbuf-2.0-0.dll
+libgdk-3-0.dll libgio-2.0-0.dll libglib-2.0-0.dll libgmodule-2.0-0.dll
+libgobject-2.0-0.dll libgraphite2.dll libgsl-19.dll libgslcblas-0.dll
+libgtk-3-0.dll libharfbuzz-0.dll libiconv-2.dll libintl-8.dll
+libobjc-4.dll libp11-kit-0.dll libpango-1.0-0.dll
 libpangocairo-1.0-0.dll libpangoft2-1.0-0.dll libpangowin32-1.0-0.dll
 libpcre-1.dll libpixman-1-0.dll libpng16-16.dll libreadline7.dll
 libstdc++-6.dll libtermcap-0.dll libwinpthread-1.dll zlib1.dll
 libngraph-0.dll ngraph.exe ngp2"
 
-mkdir $PKG_DIR
-for subdir in bin etc lib share
-do
-    mkdir $PKG_DIR/$subdir/
-    case $subdir in
-	bin)
-	    for i in $BINFILES
-	    do
-		cp $WIN_PATH/$subdir/$i $PKG_DIR/$subdir/
-	    done
-	;;
-	etc)
-	    for i in fonts gtk-3.0 ngraph-gtk
-	    do
-		cp -r $WIN_PATH/$subdir/$i $PKG_DIR/$subdir/
-	    done
-	;;
-	lib)
-	    for i in gdk-pixbuf-2.0 glib-2.0 gtk-3.0 ngraph-gtk
-	    do
-		cp -r $WIN_PATH/$subdir/$i $PKG_DIR/$subdir/
-	    done
-	;;
-	share)
-	    for i in GConf glib-2.0 icons locale themes ngraph-gtk pixmaps
-	    do
-		cp -r $WIN_PATH/$subdir/$i $PKG_DIR/$subdir/
-	    done
-	    mkdir $PKG_DIR/$subdir/doc
-	    cp -r $WIN_PATH/$subdir/doc/ngraph-gtk $PKG_DIR/$subdir/doc
-	;;
-    esac
-done
+BINFILES64="libgcc_s_seh-1.dll"
+BINFILES32="libgcc_s_dw2-1.dll"
 
-# cp src/ngraph.ico $PKG_DIR/share/icons
+make_zip() {
+    echo create win$1 archive.
+    if [ -d $PKG_DIR ]
+    then
+	rm -rf $PKG_DIR
+    fi
+
+    mkdir $PKG_DIR
+    win_path=$WIN_PATH$1
+    for subdir in bin etc lib share
+    do
+	echo "  copy $subdir."
+	mkdir $PKG_DIR/$subdir/
+	case $subdir in
+	    bin)
+		for i in $BINFILES
+		do
+		    cp $win_path/$subdir/$i $PKG_DIR/$subdir/
+		done
+		if [ $1 = "64" ]
+		then
+		    for i in $BINFILES64
+		    do
+			cp $win_path/$subdir/$i $PKG_DIR/$subdir/
+		    done
+		else
+		    for i in $BINFILES32
+		    do
+			cp $win_path/$subdir/$i $PKG_DIR/$subdir/
+		    done
+		fi
+		;;
+	    etc)
+		for i in fonts gtk-3.0 ngraph-gtk
+		do
+		    cp -r $win_path/$subdir/$i $PKG_DIR/$subdir/
+		done
+		;;
+	    lib)
+		for i in gdk-pixbuf-2.0 glib-2.0 gtk-3.0 ngraph-gtk
+		do
+		    cp -r $win_path/$subdir/$i $PKG_DIR/$subdir/
+		done
+		;;
+	    share)
+		for i in GConf glib-2.0 icons locale themes ngraph-gtk pixmaps
+		do
+		    cp -r $win_path/$subdir/$i $PKG_DIR/$subdir/
+		done
+		mkdir $PKG_DIR/$subdir/doc
+		cp -r $win_path/$subdir/doc/ngraph-gtk $PKG_DIR/$subdir/doc
+		;;
+	esac
+    done
+
+    archive=ngraph-gtk-${VERSION}-win$1.zip
+    if [ -f $archive ]
+    then
+	rm $archive
+    fi
+    echo "  archiving."
+    zip -qr9 $archive $PKG_DIR
+}
+
+for arch in 32 64
+do
+    make_zip $arch
+done
