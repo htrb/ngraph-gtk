6f97649a6a653b8218c5149e6aa5502c513ec2a3
Hiroyuki Ito <ZXB01226@nifty.com>
2018-02-08 15:45:33 +0900

    (create_application_window): don't use appmenu on Windows.

--- src/gtk/gtk_action.c
+++ src/gtk/gtk_action.c
@@ -852,7 +852,7 @@ create_application_window(GtkWidget **popup)
   g_application_register(G_APPLICATION(app), NULL, NULL);
   g_action_map_add_action_entries(G_ACTION_MAP(app), AppEntries, G_N_ELEMENTS(AppEntries), app);
 
-#if OSX || WINDOWS
+#if OSX
   {
     /* only for remove "Settings" menu item */
     GtkBuilder *builder;
