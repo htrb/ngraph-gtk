43a5900d13e93bbb6203aa2276c54a652fe774f7
Hiroyuki Ito <ZXB01226@nifty.com>
2018-02-15 16:51:30 +0900

    add completion informations and scripts.

---
+++ src/gtk/create_const_info.rb
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+data = []
+File.open(ARGV[0], "r") { |info_file|
+  info_file.each { |l|
+    data.push(l.chomp.split(/\t+/))
+  }
+}
+
+data.sort!
+File.open(ARGV[1], "w") { |c_file|
+  c_file.puts('#include "completion_info.h"')
+  c_file.puts("struct completion_info completion_info_const[] = {")
+  data.each {|info|
+    info_text = info[1].gsub('"', '\\"')
+    c_file.puts(%Q!{"#{info[0]}", "#{info_text}"},!)
+  }
+  c_file.puts("};")
+  c_file.puts("int completion_info_const_num(void) {return sizeof(completion_info_const) / sizeof(*completion_info_const);}")  
+}
+
