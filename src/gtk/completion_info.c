cfeb77dad5bd5d98bb93c7d4031f584133b9a903
Hiroyuki Ito <ZXB01226@nifty.com>
2018-02-15 16:53:12 +0900

    add original completion provider.

---
+++ src/gtk/completion_info.c
@@ -0,0 +1,33 @@
+#include "completion_info.h"
+
+GList *
+completion_info_populate(struct completion_info *info, int num, const char *word, int len)
+{
+  GList *ret = NULL;
+  GtkSourceCompletionItem *proposal;
+  int i;
+
+  for (i = 0; i < num; i++) {
+    if (g_ascii_strncasecmp(info[i].text, word, len)) {
+      continue;
+    }
+    proposal = gtk_source_completion_item_new2();
+    gtk_source_completion_item_set_label(proposal, info[i].text);
+    gtk_source_completion_item_set_text(proposal, info[i].text);
+    gtk_source_completion_item_set_info(proposal, info[i].info);
+    ret = g_list_prepend (ret, proposal);
+  }
+  return ret;
+}
+
+GList *
+completion_info_func_populate(const char *word, int len)
+{
+  return completion_info_populate(completion_info_func, completion_info_func_num(), word, len);
+}
+
+GList *
+completion_info_const_populate(const char *word, int len)
+{
+  return completion_info_populate(completion_info_const, completion_info_const_num(), word, len);
+}
---
+++ src/gtk/completion_info.h
@@ -0,0 +1,17 @@
+#ifndef COMPLETION_INFO_HEADER
+#define COMPLETION_INFO_HEADER
+#include "gtk_common.h"
+
+struct completion_info 
+{
+  char *text, *info;
+};
+
+extern struct completion_info completion_info_const[];
+extern struct completion_info completion_info_func[];
+int completion_info_func_num(void);
+int completion_info_const_num(void);
+GList *completion_info_func_populate(const char *word, int len);
+GList *completion_info_const_populate(const char *word, int len);
+
+#endif
