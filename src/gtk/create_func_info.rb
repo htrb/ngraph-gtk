e40225367a12a480e09d0a91a5c8639e8c82af88
Hiroyuki Ito <ZXB01226@nifty.com>
2018-02-17 09:30:06 +0900

    define functions and constants data in onw file.

--- src/gtk/create_func_info.rb
+++ src/gtk/create_func_info.rb
@@ -1,25 +1,62 @@
 # -*- coding: utf-8 -*-
 
-data = []
-File.open(ARGV[0], "r:utf-8") { |info_file|
-  info_file.each { |l|
-    info = l.chomp.split(/\t+/)
+class CompletionInfo
+  DataTypeFunc  = 0
+  DataTypeConst = 1
+
+  def initialize(func_file, const_file)
+    @func_data = []
+    @const_data = []
+    load_data(DataTypeFunc, func_file, @func_data)
+    load_data(DataTypeConst, const_file, @const_data)
+  end
+
+  def func_struct(info)
     text = info[0].sub(/\(.+\)/, '()')
     ltext = text.downcase
     info_text = info[1].gsub('"', '\\"')
-    data.push(%Q!{"#{ltext}", "#{text}", N_("#{info[0]}\\n#{info_text}"), NULL},!)
-  }
-}
+    %Q!{"#{ltext}", "#{text}", N_("#{info[0]}\\n#{info_text}"), NULL},!
+  end
 
-data.sort!
-File.open(ARGV[1], "w:utf-8") { |c_file|
-  c_file.puts('/* -*- Mode: C; coding: utf-8 -*- */')
-  c_file.puts('#include "gtk_common.h"')
-  c_file.puts('#include "completion_info.h"')
-  c_file.puts("struct completion_info completion_info_func[] = {")
-  data.each {|info|
-    c_file.puts(info)
-  }
-  c_file.puts("};")
-  c_file.puts("int completion_info_func_num(void) {return sizeof(completion_info_func) / sizeof(*completion_info_func);}")
-}
+  def const_struct(info)
+    info_text = info[1].gsub('"', '\\"')
+    %Q!{"#{info[0].downcase}", "#{info[0]}", N_("#{info_text}"), NULL},!
+  end
+
+  def load_data(type, file, ary)
+    File.open(file, "r:utf-8") { |info_file|
+      info_file.each { |l|
+        info = l.chomp.split(/\t+/)
+        data = if (type == DataTypeFunc)
+                 func_struct(info)
+               else
+                 const_struct(info)
+               end
+        ary.push(data)
+      }
+    }
+    ary.sort!
+  end
+
+  def save_sruct(type, data, c_file)
+    c_file.puts("struct completion_info completion_info_#{type}[] = {")
+    data.each {|info|
+      c_file.puts(info)
+    }
+    c_file.puts("{NULL, NULL, NULL, NULL}")
+    c_file.puts("};")
+  end
+
+  def save_data(filename)
+    File.open(filename, "w:utf-8") { |c_file|
+      c_file.puts('/* -*- Mode: C; coding: utf-8 -*- */')
+      c_file.puts('#include "gtk_common.h"')
+      c_file.puts('#include "completion_info.h"')
+      save_sruct("func", @func_data, c_file)
+      save_sruct("const", @const_data, c_file)
+    }
+  end
+end
+
+completion_info = CompletionInfo.new(ARGV[0], ARGV[1])
+completion_info.save_data(ARGV[2])
