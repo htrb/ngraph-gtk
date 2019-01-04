4a55731bb320079f1f7f7ea414068ebd0deed018
Hiroyuki Ito <ZXB01226@nifty.com>
2019-01-04 18:55:15 +0900

    * gtk_presettings.h: add prototype of the function "presetting_set_parameters()".

--- src/gtk/gtk_presettings.h
+++ src/gtk/gtk_presettings.h
@@ -7,6 +7,7 @@
 GtkWidget *presetting_create_panel(GtkApplication *app);
 void presetting_set_obj_field(struct objlist *obj, int id);
 void presetting_set_visibility(enum PointerType type);
+void presetting_set_parameters(struct Viewer *d);
 void presetting_show_all(void);
 
 #endif	/* GTK_PRESETTINGS_HEADER */
