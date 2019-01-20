1686fd883a37631a3b80cbae990d435aa5afa8ca
Hiroyuki Ito <ZXB01226@nifty.com>
2019-01-20 21:04:43 +0900

    * gtk_presettings.h: add "presetting_show_focused()".

--- src/gtk/gtk_presettings.h
+++ src/gtk/gtk_presettings.h
@@ -8,6 +8,6 @@ GtkWidget *presetting_create_panel(GtkApplication *app);
 void presetting_set_obj_field(struct objlist *obj, int id);
 void presetting_set_visibility(enum PointerType type);
 void presetting_set_parameters(struct Viewer *d);
-void presetting_show_all(void);
+void presetting_show_focused(void);
 
 #endif	/* GTK_PRESETTINGS_HEADER */
