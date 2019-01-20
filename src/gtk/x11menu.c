7b1584f419c428ae24c819294c896023c409913b
Hiroyuki Ito <ZXB01226@nifty.com>
2019-01-20 21:06:02 +0900

    * x11menu.c (set_toolbox_mode): call "presetting_show_focused()".

--- src/gtk/x11menu.c
+++ src/gtk/x11menu.c
@@ -5485,7 +5485,7 @@ set_toolbox_mode(enum TOOLBOX_MODE mode)
     widget = CToolbar;
     break;
   case TOOLBOX_MODE_SETTING_PANEL:
-    presetting_show_all();
+    presetting_show_focused();
     widget = SettingPanel;
     break;
   }
