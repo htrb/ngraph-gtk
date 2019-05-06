6e2b8f8a6c6aada8717e395b11f0f686d23b2360
Hiroyuki Ito <ZXB01226@nifty.com>
2019-05-06 15:45:32 +0900

    * math_scanner.c (math_scanner_free_token): free string of a string variable.

--- src/math/math_scanner.c
+++ src/math/math_scanner.c
@@ -79,13 +79,11 @@ math_scanner_free_token(struct math_token *token)
   case MATH_TOKEN_TYPE_UNTERMINATED_STRING:
     break;
   case MATH_TOKEN_TYPE_SYMBOL:
+  case MATH_TOKEN_TYPE_STRING_VARIABLE:
     g_free(token->data.sym);
     break;
   case MATH_TOKEN_TYPE_STRING:
     g_string_free(token->data.str, TRUE);
-    break;
-  case MATH_TOKEN_TYPE_STRING_VARIABLE:
-    /* not implemented yet */
     break;
   }
 
