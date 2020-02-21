6db7e1d0e8cd02a10f8f71929fb9192e99f87311
Hiroyuki Ito <ZXB01226@nifty.com>
2020-02-21 14:21:02 +0900

    * ogra.c (oGRAclear): return when the variable "device" is NULL.

--- src/ogra.c
+++ src/ogra.c
@@ -325,12 +325,13 @@ oGRAclear(struct objlist *obj,N_VALUE *inst,N_VALUE *rval,int argc,char **argv)
   if (GC!=-1) GRAreopen(GC);
   _getobj(obj,"oid",inst,&oid);
   _getobj(obj,"_device",inst,&device);
-  if (device!=NULL) {
+  if (device == NULL) {
+    return 0;
+  }
     if (((dobj=getobjlist(device,&did,&dfield,NULL))!=NULL)
     && ((dinst=chkobjinstoid(dobj,did))!=NULL)) {
       if (chkobjfield(dobj,field)==0) _exeobj(dobj,field,dinst,0,NULL);
     }
-  }
   return 0;
 }
 
