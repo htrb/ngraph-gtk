5d0c383bee0e666bae9f407f236f407ed1746129
Hiroyuki Ito <ZXB01226@nifty.com>
2020-02-21 13:32:08 +0900

    * osystem.c (syshideinstance): return when the variable "array" is NULL.

--- src/osystem.c
+++ src/osystem.c
@@ -380,7 +380,9 @@ syshideinstance(struct objlist *obj,N_VALUE *inst,N_VALUE *rval,int argc,char **
   int j,anum;
 
   array=(struct narray *)argv[2];
-  if (array!=NULL) {
+  if (array == NULL) {
+    return 0;
+  }
     anum=arraynum(array);
     objdata=arraydata(array);
     for (j=0;j<anum;j++) {
@@ -388,7 +390,6 @@ syshideinstance(struct objlist *obj,N_VALUE *inst,N_VALUE *rval,int argc,char **
       obj2=getobject(objdata[j]);
       if ((obj2!=obj) && (obj2!=NULL)) hideinstance(obj2);
     }
-  }
   return 0;
 }
 
