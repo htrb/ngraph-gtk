a03cf2e08c2453215e96156ff0e66e7074cc377e
Hiroyuki Ito <ZXB01226@nifty.com>
2020-02-21 13:53:21 +0900

    * osystem.c (systemresize): return when the variable "num" is less than 2.

--- src/osystem.c
+++ src/osystem.c
@@ -423,10 +423,11 @@ systemresize(struct objlist *obj,N_VALUE *inst,N_VALUE *rval,int argc,char **arg
 
   iarray=(struct narray *)argv[2];
   num=arraynum(iarray);
-  if (num >= 2) {
+  if (num < 2) {
+    return 0;
+  }
     data=arraydata(iarray);
     resizeconsole(data[0], data[1]);
-  }
   return 0;
 }
 
