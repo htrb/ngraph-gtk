5d38d5d473e49a9a7585fb4e0f0cc72fbf102388
Hiroyuki Ito <ZXB01226@nifty.com>
2020-07-07 17:03:00 +0900

    * x11parameter.c (parameter_up): now the real type of the 2nd parameter is "struct parameter_data *".

--- src/gtk/x11parameter.c
+++ src/gtk/x11parameter.c
@@ -510,17 +510,15 @@ parameter_update(GtkButton *btn, gpointer data)
 static void
 parameter_up(GtkButton *btn, gpointer data)
 {
-  int id;
-  struct obj_list_data *d;
+  struct parameter_data *d;
 
   if (Menulock || Globallock)
     return;
 
-  d = NgraphApp.ParameterWin.data.data;
-  id = GPOINTER_TO_INT(data);
+  d = data;
   menu_save_undo_single(UNDO_TYPE_ORDER, d->obj->name);
-  moveupobj(d->obj, id);
-  ParameterWinUpdate(NgraphApp.ParameterWin.data.data, FALSE, FALSE);
+  moveupobj(d->obj, d->id);
+  ParameterWinUpdate(d->obj_list_data, FALSE, FALSE);
   set_graph_modified();
 }
 
