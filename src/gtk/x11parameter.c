b69844e48c2d597b21c9815c721cd3abf1c6df41
Hiroyuki Ito <ZXB01226@nifty.com>
2020-07-07 16:58:15 +0900

    * x11parameter.c (add_toggle_button): added.

--- src/gtk/x11parameter.c
+++ src/gtk/x11parameter.c
@@ -702,6 +702,14 @@ add_button(GtkWidget *grid, int row, int col, const char *icon, const char *tool
   return w;
 }
 
+static GtkWidget *
+add_toggle_button(GtkWidget *grid, int row, int col, const char *icon_name, const char *tooltip, GCallback proc, gpointer data)
+{
+  GtkWidget *w, *icon;
+  w = gtk_toggle_button_new();
+  icon = gtk_image_new_from_icon_name(icon_name, GTK_ICON_SIZE_BUTTON);
+  gtk_button_set_image(GTK_BUTTON(w), icon);
+  add_button_common(w, grid, row, col, tooltip, proc, data);
   return w;
 }
 
