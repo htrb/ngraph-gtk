a7f87611dfe06c2e1b90e42599257dab9f7c373e
Hiroyuki Ito <ZXB01226@nifty.com>
2020-07-08 14:34:36 +0900

    * x11parameter.h: add prototypes of the functions.

--- src/gtk/x11parameter.h
+++ src/gtk/x11parameter.h
@@ -1,3 +1,5 @@
 GtkWidget *create_parameter_list(struct SubWin *d);
 void CmParameterAdd(void *w, gpointer client_data);
+void CmParameterDelete(void *w, gpointer client_data);
+void CmParameterUpdate(void *w, gpointer client_data);
 void ParameterWinUpdate(struct obj_list_data *d, int clear, int draw);
