2530478767606fc89f46345c06d32d9c36c090cc
Hiroyuki Ito <ZXB01226@nifty.com>
2020-08-20 18:31:26 +0900

    * strconv.h: fix type of the functions.

--- src/strconv.h
+++ src/strconv.h
@@ -2,6 +2,6 @@
  * $Id: strconv.h,v 1.1 2009-11-16 09:13:04 hito Exp $
  */
 
-char *sjis_to_utf8(char *src);
-char *utf8_to_sjis(char *src);
+char *sjis_to_utf8(const char *src);
+char *utf8_to_sjis(const char *src);
 gchar *ascii2greece(const gchar *src);
