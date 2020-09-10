0dcb60ac1efd2a5de949926f55fe44d793d2c1bc
Hiroyuki Ito <ZXB01226@nifty.com>
2020-09-10 20:05:08 +0900

    * odata.c (line_number): return 0 when user-data is NULL (for a constant definition).

--- src/odata.c
+++ src/odata.c
@@ -542,7 +542,7 @@ line_number(MathFunctionCallExpression *exp, MathEquation *eq, MathValue *rval)
   fp = math_equation_get_user_data(eq);
   if (fp == NULL) {
     rval->type = MATH_VALUE_ERROR;
-    return 1;
+    return 0;
   }
 
   rval->val = fp->line;
