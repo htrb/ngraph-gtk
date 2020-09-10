370eeec703b73851f421e48bb4e2518c0eb01570
Hiroyuki Ito <ZXB01226@nifty.com>
2020-09-10 20:08:31 +0900

    * odata.c (file_draw_arc): return 0 when user-data is NULL (for a constant definition).

--- src/odata.c
+++ src/odata.c
@@ -1103,7 +1103,7 @@ file_draw_arc(MathFunctionCallExpression *exp, MathEquation *eq, MathValue *rval
   fp = math_equation_get_user_data(eq);
   if (fp == NULL) {
     rval->type = MATH_VALUE_ERROR;
-    return 1;
+    return 0;
   }
 
   if (fp->GC < 0) {
