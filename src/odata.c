3dfc7dad1d94bee32c5a6a164859f98826208871
Hiroyuki Ito <ZXB01226@nifty.com>
2020-09-10 20:09:33 +0900

    * odata.c (file_draw_errorbar2): return 0 when user-data is NULL (for a constant definition).

--- src/odata.c
+++ src/odata.c
@@ -1383,7 +1383,7 @@ file_draw_errorbar2(MathFunctionCallExpression *exp, MathEquation *eq, MathValue
   fp = math_equation_get_user_data(eq);
   if (fp == NULL) {
     rval->type = MATH_VALUE_ERROR;
-    return 1;
+    return 0;
   }
 
   if (fp->GC < 0) {
