a740cd292adbc246c3f93f4a900630af53cc4451
Hiroyuki Ito <ZXB01226@nifty.com>
2020-09-10 20:07:33 +0900

    * odata.c (file_rgb_sub): return 0 when user-data is NULL (for a constant definition).

--- src/odata.c
+++ src/odata.c
@@ -803,7 +803,7 @@ file_rgb_sub(MathFunctionCallExpression *exp, MathEquation *eq, MathValue *rval,
   fp = math_equation_get_user_data(eq);
   if (fp == NULL) {
     rval->type = MATH_VALUE_ERROR;
-    return 1;
+    return 0;
   }
 
   r = exp->buf[0].val.val * 255;
