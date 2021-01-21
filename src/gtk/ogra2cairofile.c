b18d00cdc77727017f7eca130e97e07679e0ae0f
Hiroyuki Ito <ZXB01226@nifty.com>
2021-01-21 15:44:22 +0900

    * ogra2cairofile.c (create_cairo): don't convert the filename on Windows.

--- src/gtk/ogra2cairofile.c
+++ src/gtk/ogra2cairofile.c
@@ -153,11 +153,7 @@ create_cairo(struct objlist *obj, N_VALUE *inst, char *fname, int iw, int ih, in
   int format, dpi, r;
   struct gra2cairo_local *local;
 
-#if WINDOWS
-  fname = g_locale_from_utf8(fname, -1, NULL, NULL, NULL);
-#else  /* WINDOWS */
   fname = g_filename_from_utf8(fname, -1, NULL, NULL, NULL);
-#endif	/* WINDOWS */
 
   if (fname == NULL) {
     *err = CAIRO_STATUS_NO_MEMORY;
