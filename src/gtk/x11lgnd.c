161c64c69b3a3dfb256cf00a1dbfd0d1fd882d6a
Hiroyuki Ito <ZXB01226@nifty.com>
2021-05-13 17:28:25 +0900

    * x11lgnd.c: support GTK 3.24 or later.

--- src/gtk/x11lgnd.c
+++ src/gtk/x11lgnd.c
@@ -1095,9 +1095,6 @@ points_setup(struct LegendDialog *d)
 
   list = GTK_TREE_MODEL(gtk_list_store_new(POINTS_DIMENSION, G_TYPE_DOUBLE, G_TYPE_DOUBLE));
   tree_view = gtk_tree_view_new_with_model(list);
-#if ! GTK_CHECK_VERSION(3, 14, 0)
-  gtk_tree_view_set_rules_hint(GTK_TREE_VIEW(tree_view), TRUE);
-#endif
   gtk_tree_view_set_rubber_banding(GTK_TREE_VIEW(tree_view), FALSE);
   gtk_tree_view_set_grid_lines(GTK_TREE_VIEW(tree_view), GTK_TREE_VIEW_GRID_LINES_VERTICAL);
   gtk_tree_view_set_reorderable(GTK_TREE_VIEW(tree_view), TRUE);
@@ -1303,25 +1300,12 @@ static void
 LegendArrowDialogScale(GtkWidget *w, struct LegendDialog *d)
 {
   GdkWindow *win;
-#if ! GTK_CHECK_VERSION(3, 22, 0)
-  cairo_t *cr;
-#endif
 
   draw_arrow_pixmap(w, d);
-#if GTK_CHECK_VERSION(3, 22, 0)
   win = gtk_widget_get_window(d->view);
   if (win) {
     gdk_window_invalidate_rect(win, NULL, FALSE);
   }
-#else  /*  GTK_CHECK_VERSION(3, 22, 0) */
-  win = gtk_widget_get_window(d->view);
-  if (win == NULL) {
-    return;
-  }
-  cr = gdk_cairo_create(win);
-  LegendArrowDialogPaint(d->view, cr, d);
-  cairo_destroy(cr);
-#endif  /*  GTK_CHECK_VERSION(3, 22, 0) */
 }
 
 static void
