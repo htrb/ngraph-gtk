4a4c36a0e57eabd2649233d841d8d31465d6479a
Hiroyuki Ito <ZXB01226@nifty.com>
2021-08-10 18:29:08 +0900

    * x11dialg.c (axis_combo_box_setup): use GtkTreeStore instead of GtkListStore.

--- src/gtk/x11dialg.c
+++ src/gtk/x11dialg.c
@@ -1373,17 +1373,24 @@ axis_combo_box_setup(GtkWidget *cbox, struct objlist *obj, int id, const char *f
   struct objlist *aobj;
   char *name;
   int aid, lastinst, self;
-  GtkListStore  *list;
+  GtkTreeStore  *list;
+  struct axis_combo_box_iter axis_iter[] = {
+    {'X', FALSE},
+    {'Y', FALSE},
+    {'U', FALSE},
+    {'R', FALSE},
+    {'\0', FALSE},		/* sentinel value */
+  };
 
-  combo_box_clear(cbox);
-  list = GTK_LIST_STORE(gtk_combo_box_get_model(GTK_COMBO_BOX(cbox)));
+  axis_combo_box_clear(cbox);
+  list = GTK_TREE_STORE(gtk_combo_box_get_model(GTK_COMBO_BOX(cbox)));
 
   aobj = getobject("axis");
   lastinst = chkobjlastinst(aobj);
   if (axis_combo_box_get_flags(cbox) & AXIS_COMBO_BOX_ADD_NONE) {
     GtkTreeIter iter;
-    gtk_list_store_append(list, &iter);
-    gtk_list_store_set(list, &iter,
+    gtk_tree_store_append(list, &iter, NULL);
+    gtk_tree_store_set(list, &iter,
 		       AXIS_COMBO_BOX_COLUMN_TITLE, _("none"),
 		       AXIS_COMBO_BOX_COLUMN_ID, -1,
 		       AXIS_COMBO_BOX_COLUMN_OID, -1,
@@ -1392,15 +1399,19 @@ axis_combo_box_setup(GtkWidget *cbox, struct objlist *obj, int id, const char *f
   }
   self = (aobj == obj) ? id : -1;
   for (aid = 0; aid <= lastinst; aid++) {
-    GtkTreeIter iter;
+    GtkTreeIter iter, *parent;
     int aoid;
 
     getobj(aobj, "group", aid, 0, NULL, &name);
     getobj(aobj, "oid", aid, 0, NULL, &aoid);
     name = CHK_STR(name);
 
-    gtk_list_store_append(list, &iter);
-    gtk_list_store_set(list, &iter,
+    parent = NULL;
+    if (lastinst > AXIS_SELECTION_LIMIT) {
+      parent = axis_combo_box_get_parent(axis_iter, list, name[1]);
+    }
+    gtk_tree_store_append(list, &iter, parent);
+    gtk_tree_store_set(list, &iter,
 		       AXIS_COMBO_BOX_COLUMN_TITLE, name,
 		       AXIS_COMBO_BOX_COLUMN_ID, aid,
 		       AXIS_COMBO_BOX_COLUMN_OID, aoid,
