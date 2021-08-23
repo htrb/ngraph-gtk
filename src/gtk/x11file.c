a1287fd4d499ef04e205dc8fdcc96317344d0607
Hiroyuki Ito <ZXB01226@nifty.com>
2021-08-23 16:54:51 +0900

    * x11file.c (MathDialogList): use "g_list_free_full()".

--- src/gtk/x11file.c
+++ src/gtk/x11file.c
@@ -583,8 +583,7 @@ MathDialogList(GtkButton *w, gpointer client_data)
   }
 
  END:
-  g_list_foreach(list, free_tree_path_cb, NULL);
-  g_list_free(list);
+  g_list_free_full(list, (GDestroyNotify) gtk_tree_path_free);
 }
 
 static void
