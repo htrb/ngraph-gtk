73ad6f8bbc0d28a2a330d83a0a2deb06d8030c6f
Hiroyuki Ito <ZXB01226@nifty.com>
2021-08-23 16:55:54 +0900

    * x11graph.c (SwitchDialogInsert): use "g_list_free_full()".

--- src/gtk/x11graph.c
+++ src/gtk/x11graph.c
@@ -377,8 +377,7 @@ SwitchDialogInsert(GtkWidget *w, gpointer client_data)
   }
 
   if (list) {
-    g_list_foreach(list, free_tree_path_cb, NULL);
-    g_list_free(list);
+    g_list_free_full(list, (GDestroyNotify) gtk_tree_path_free);
   }
 
   SwitchDialogSetupItem(d->widget, d);
