c751149a2f9e778b33533430b9f0952eb6680f76
Hiroyuki Ito <ZXB01226@nifty.com>
2021-09-26 20:34:23 +0900

    * gtk_presettings.c (create_menu_button): don't use popover.

--- src/gtk/gtk_presettings.c
+++ src/gtk/gtk_presettings.c
@@ -1761,6 +1761,7 @@ create_menu_button(GtkBuilder *builder, const char *menu_name, const char *toolt
   GMenuModel *menu;
   w = gtk_menu_button_new();
   menu = G_MENU_MODEL(gtk_builder_get_object(builder, menu_name));
+  gtk_menu_button_set_use_popover(GTK_MENU_BUTTON(w), FALSE);
   gtk_menu_button_set_menu_model(GTK_MENU_BUTTON(w), menu);
   gtk_widget_set_tooltip_text(w, tooltip);
   return w;
