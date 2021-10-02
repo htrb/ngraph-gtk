fa9819373b5cebb653f1bef84b6dbbde7b373de2
Hiroyuki Ito <ZXB01226@nifty.com>
2021-10-02 20:29:05 +0900

    * ogra2cairo.h (gra2cairo_local): add a member "force_opacity".

--- src/gtk/ogra2cairo.h
+++ src/gtk/ogra2cairo.h
@@ -39,6 +39,7 @@ struct gra2cairo_local {
   cairo_t *cairo;
   PangoLayout *layout;
   int linetonum, text2path, antialias, use_opacity;
+  double force_opacity;
   struct fontmap *loadfont;
   char *fontalias;
   int font_style, symbol;
