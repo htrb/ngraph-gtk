1bd47c5721bc4504e2e187204f82cbf62b04df6e
Hiroyuki Ito <ZXB01226@nifty.com>
2021-10-03 12:24:25 +0900

    * ogra2cairo.c (gra2cairo_output): fix indent.

--- src/gtk/ogra2cairo.c
+++ src/gtk/ogra2cairo.c
@@ -1134,18 +1134,18 @@ gra2cairo_output(struct objlist *obj, N_VALUE *inst, N_VALUE *rval,
     cairo_set_line_join(local->cairo, join);
     break;
   case 'G':
-      if (local->use_opacity && cpar[0] > 3 && cpar[4] < 255) {
-	cairo_set_source_rgba(local->cairo,
-			      cpar[1] / 255.0,
-			      cpar[2] / 255.0,
-			      cpar[3] / 255.0,
-			      cpar[4] / 255.0);
-      } else {
-	cairo_set_source_rgb(local->cairo,
-			     cpar[1] / 255.0,
-			     cpar[2] / 255.0,
-			     cpar[3] / 255.0);
-      }
+    if (local->use_opacity && cpar[0] > 3 && cpar[4] < 255) {
+      cairo_set_source_rgba(local->cairo,
+			    cpar[1] / 255.0,
+			    cpar[2] / 255.0,
+			    cpar[3] / 255.0,
+			    cpar[4] / 255.0);
+    } else {
+      cairo_set_source_rgb(local->cairo,
+			   cpar[1] / 255.0,
+			   cpar[2] / 255.0,
+			   cpar[3] / 255.0);
+    }
     break;
   case 'M':
     cairo_move_to(local->cairo, mxd2px(local, cpar[1]), mxd2py(local, cpar[2]));
