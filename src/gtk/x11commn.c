d1ea03120f38068c5b6a46572755f88cbea65a6b
Hiroyuki Ito <ZXB01226@nifty.com>
2021-12-02 15:09:50 +0900

    * x11commn.c (ProgressDialogCreate): stop using "gtk_window_set_position()".

--- src/gtk/x11commn.c
+++ src/gtk/x11commn.c
@@ -2002,8 +2002,8 @@ ProgressDialogCreate(char *title)
 
   gtk_window_set_transient_for(GTK_WINDOW(ProgressDialog), GTK_WINDOW(TopLevel));
   gtk_window_set_modal(GTK_WINDOW(ProgressDialog), TRUE);
+#if ! GTK_CHECK_VERSION(4, 0, 0)
   gtk_window_set_position(GTK_WINDOW(ProgressDialog), GTK_WIN_POS_CENTER);
-#if GTK_CHECK_VERSION(4, 0, 0)
   gtk_window_set_type_hint(GTK_WINDOW(ProgressDialog), GDK_WINDOW_TYPE_HINT_DIALOG);
 #endif
 
