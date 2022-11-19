40a8f8db587f818c72d7c8c1876cd76c4e895c5c
Hiroyuki Ito <ZXB01226@nifty.com>
2022-11-19 10:53:16 +0900

    * x11file.c (mask_tab_create): use "spin_button_set_activates_signal()".

--- src/gtk/x11file.c
+++ src/gtk/x11file.c
@@ -2486,7 +2486,9 @@ mask_tab_create(struct FileDialog *d)
 
   i = 0;
   w = create_spin_entry_type(SPIN_BUTTON_TYPE_NATURAL, TRUE, FALSE);
-#if ! GTK_CHECK_VERSION(4, 0, 0)
+#if GTK_CHECK_VERSION(4, 0, 0)
+  spin_button_set_activates_signal(w, G_CALLBACK(FileMaskDialogAdd), d);
+#else
   g_signal_connect(w, "activate", G_CALLBACK(FileMaskDialogAdd), d);
 #endif
   add_widget_to_table(table, w, _("_Line:"), FALSE, i++);
