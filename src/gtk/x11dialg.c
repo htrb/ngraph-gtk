fc571cf9e8d96c68163662c8937ba91c73a05715
Hiroyuki Ito <ZXB01226@nifty.com>
2023-03-12 17:30:19 +0900

    * x11dialg.c (SetObjFieldFromWidget): drop GTK3 support.

--- src/gtk/x11dialg.c
+++ src/gtk/x11dialg.c
@@ -803,20 +803,14 @@ SetObjFieldFromWidget(GtkWidget *w, struct objlist *Obj, int Id, char *field)
     r = SetObjFieldFromText(w, Obj, Id, field);
   } else if (G_TYPE_CHECK_INSTANCE_TYPE(w, GTK_TYPE_COMBO_BOX)) {
     if (gtk_combo_box_get_has_entry(GTK_COMBO_BOX(w))) {
-#if GTK_CHECK_VERSION(4, 0, 0)
       r = SetObjFieldFromText(gtk_combo_box_get_child(GTK_COMBO_BOX(w)), Obj, Id, field);
-#else
-      r = SetObjFieldFromText(gtk_bin_get_child(GTK_BIN(w)), Obj, Id, field);
-#endif
     } else {
       r = SetObjFieldFromList(w, Obj, Id, field);
     }
   } else if (G_TYPE_CHECK_INSTANCE_TYPE(w, GTK_TYPE_TOGGLE_BUTTON)) {
     r = SetObjFieldFromToggle(w, Obj, Id, field);
-#if GTK_CHECK_VERSION(4, 0, 0)
   } else if (G_TYPE_CHECK_INSTANCE_TYPE(w, GTK_TYPE_CHECK_BUTTON)) {
     r = SetObjFieldFromToggle(w, Obj, Id, field);
-#endif
   }
 
   if (r) {
