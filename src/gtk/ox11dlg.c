4066b2a33161b91e7fee5f8b756755e4549eaa24
Hiroyuki Ito <ZXB01226@nifty.com>
2023-04-30 13:46:09 +0900

    * ox11dlg.c (dlggetopenfile_response): call "dlg_response()".

--- src/gtk/ox11dlg.c
+++ src/gtk/ox11dlg.c
@@ -714,15 +714,13 @@ dlgbeep(struct objlist *obj, N_VALUE *inst, N_VALUE *rval, int argc, char **argv
   return 0;
 }
 
-#if GTK_CHECK_VERSION(4, 0, 0)
 static void
 dlggetopenfile_response(char *file, gpointer user_data)
 {
   struct dialog_data *data;
   data = (struct dialog_data *) user_data;
+  dlg_response((file) ? IDOK : IDCANCEL, data);
   data->response_text = file;
-  data->response = (file) ? IDOK : IDCANCEL;
-  data->wait = FALSE;
 }
 
 static void
@@ -732,7 +730,6 @@ dlggetopenfile_main(gpointer user_data)
   data = (struct dialog_data *) user_data;
   nGetOpenFileName(get_toplevel_window(), data->title, data->defext, NULL, data->initial_text, FALSE, dlggetopenfile_response, data);
 }
-#endif
 
 static int
 dlggetopenfile(struct objlist *obj, N_VALUE *inst, N_VALUE *rval,
