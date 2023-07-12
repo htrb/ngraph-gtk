eef554b8c37582e2ebed71d745ec95bebf3ce766
Hiroyuki Ito <ZXB01226@nifty.com>
2023-07-12 13:48:15 +0900

    * sourcecompletionwords.h: drop GtkSourceview4 support.

--- src/gtk/sourcecompletionwords.h
+++ src/gtk/sourcecompletionwords.h
@@ -51,20 +51,14 @@ struct _SourceCompletionWordsClass {
 
 GType source_completion_words_get_type (void) G_GNUC_CONST;
 
-#if GTK_CHECK_VERSION(4, 0, 0)
 typedef GListStore * (* populate_func)(const char *, int, GtkTextIter *);
-#else
-typedef GList * (* populate_func)(const char *, int, GtkTextIter *);
-#endif
 SourceCompletionWords *source_completion_words_new(const gchar *name, populate_func func);
 
-#if GTK_CHECK_VERSION(4, 0, 0)
 struct _WordsProposal;
 typedef struct _WordsProposal WordsProposal;
 WordsProposal *words_proposal_new (void);
 void words_proposal_set_text (WordsProposal *item, const char *text);
 void words_proposal_set_info (WordsProposal *item, const char *info);
-#endif
 G_END_DECLS
 
 #endif /* SOURCE_COMPLETION_WORDS_H */
