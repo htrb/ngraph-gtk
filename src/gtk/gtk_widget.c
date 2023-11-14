637acb7f569acb58874fe10d07cc0a9845158234
Hiroyuki Ito <ZXB01226@nifty.com>
2023-11-14 15:01:44 +0900

    * gtk_widget.c (setup_popup_menu_cb): added.

--- src/gtk/gtk_widget.c
+++ src/gtk/gtk_widget.c
@@ -12,6 +12,16 @@
 #include "x11gui.h"
 #include "dir_defs.h"
 
+void
+setup_popup_menu_cb (GtkListItemFactory *factory, GtkListItem *list_item)
+{
+  GtkWidget *label;
+
+  label = gtk_label_new (NULL);
+  gtk_widget_set_halign (label, GTK_ALIGN_START);
+  gtk_list_item_set_child (list_item, label);
+}
+
 GtkWidget *
 button_new_with_icon(const char *icon_name, int toggle)
 {
