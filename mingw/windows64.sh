e45179a6ffc4ea07039d67f80f61f51432b838ce
Hiroyuki Ito <ZXB01226@nifty.com>
2024-04-08 08:51:13 +0900

    * windows64.sh (BINFILES): updated.

--- mingw/windows64.sh
+++ mingw/windows64.sh
@@ -13,23 +13,27 @@ MO_FILES="gtk30.mo ngraph-gtk.mo"
 
 VERSION=`basename $ARCHIVE '.tar.gz' | sed -e 's/ngraph-gtk-//'`
 
-BINFILES=" libatk-1.0-0.dll libbrotlicommon.dll libbrotlidec.dll
-libbz2-1.dll libcairo-2.dll libcairo-gobject-2.dll libdatrie-1.dll
-libepoxy-0.dll libexpat-1.dll libffi-8.dll libfontconfig-1.dll
-libfreetype-6.dll libfribidi-0.dll libgdk-3-0.dll
-libgdk_pixbuf-2.0-0.dll libgio-2.0-0.dll libglib-2.0-0.dll
-libgmodule-2.0-0.dll libgobject-2.0-0.dll libgraphite2.dll
-libgsl-27.dll libgslcblas-0.dll libgtk-3-0.dll
-libgtksourceview-4-0.dll libharfbuzz-0.dll libiconv-2.dll
-libintl-8.dll liblzma-5.dll libpango-1.0-0.dll libpangocairo-1.0-0.dll
-libpangoft2-1.0-0.dll libpangowin32-1.0-0.dll libpcre2-8-0.dll
-libpdcurses.dll libpixman-1-0.dll libpng16-16.dll libreadline8.dll
-librsvg-2-2.dll libtermcap-0.dll libthai-0.dll libwinpthread-1.dll
-libxml2-2.dll zlib1.dll libngraph-0.dll gdbus.exe ngraph.exe ngp2"
+BINFILES="libLerc.dll libatk-1.0-0.dll libbz2-1.dll libcairo-2.dll
+          libcairo-gobject-2.dll libcairo-script-interpreter-2.dll
+          libdatrie-1.dll libdeflate.dll libepoxy-0.dll libexpat-1.dll
+          libffi-8.dll libfontconfig-1.dll libfreetype-6.dll
+          libfribidi-0.dll libgdk_pixbuf-2.0-0.dll libgio-2.0-0.dll
+          libglib-2.0-0.dll libgmodule-2.0-0.dll libgobject-2.0-0.dll
+          libgraphene-1.0-0.dll libgraphite2.dll libgsl-27.dll
+          libgslcblas-0.dll libgtk-4-1.dll libgtksourceview-5-0.dll
+          libharfbuzz-0.dll libiconv-2.dll libintl-8.dll libjbig-0.dll
+          libjpeg-8.dll liblzma-5.dll liblzo2-2.dll libpango-1.0-0.dll
+          libpangocairo-1.0-0.dll libbrotlidec.dll libbrotlicommon.dll
+          libpangoft2-1.0-0.dll libpangowin32-1.0-0.dll
+          libpcre2-8-0.dll libpixman-1-0.dll libpng16-16.dll
+          libreadline8.dll librsvg-2-2.dll libsharpyuv-0.dll
+          libtermcap-0.dll libthai-0.dll libtiff-6.dll libwebp-7.dll
+          libwinpthread-1.dll libxml2-2.dll libzstd.dll zlib1.dll
+          libngraph-0.dll gdbus.exe ngraph.exe ngp2"
 
 BINFILES64="libgcc_s_seh-1.dll gspawn-win64-helper-console.exe libstdc++-6.dll"
 BINFILES32="libgcc_s_dw2-1.dll gspawn-win32-helper-console.exe libstdc++-6.dll"
-BINFILESARM="libclang.dll gspawn-win64-helper-console.exe libbrotlicommon.dll libbrotlidec.dll libc++.dll libunwind.dll"
+BINFILESARM="libclang.dll gspawn-win64-helper-console.exe libc++.dll libunwind.dll"
 
 make_zip() {
     echo create $1 archive.
