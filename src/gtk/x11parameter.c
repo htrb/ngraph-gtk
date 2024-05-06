74aacb43ac99e0321b84650600d9e10f6d2587eb
Hiroyuki Ito <ZXB01226@nifty.com>
2024-05-06 19:42:01 +0900

    * x11parameter.c (parameter_type_changed): use gtk_widget_set_visible instead of gtk_widget_hide/show.

--- src/gtk/x11parameter.c
+++ src/gtk/x11parameter.c
@@ -269,11 +269,11 @@ parameter_type_changed(GtkWidget *combo, GParamSpec *spec, gpointer user_data)
   }
   switch (type) {
   case PARAMETER_TYPE_SPIN:
-    gtk_widget_show(d->wrap);
+    gtk_widget_set_visible(d->wrap, TRUE);
     gtk_stack_set_visible_child_name(GTK_STACK(d->stack), TYPE_SPIN_NAME);
     break;
   case PARAMETER_TYPE_SCALE:
-    gtk_widget_hide(d->wrap);
+    gtk_widget_set_visible(d->wrap, FALSE);
     gtk_stack_set_visible_child_name(GTK_STACK(d->stack), TYPE_SPIN_NAME);
     break;
   case PARAMETER_TYPE_SWITCH:
