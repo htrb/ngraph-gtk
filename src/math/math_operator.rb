d0ad0c3a7cbcb56267c2cc8ef998df278d31d725
Hiroyuki Ito <ZXB01226@nifty.com>
2024-06-03 15:36:22 +0900

    * math_operator.rb: RuboCop correction.

--- src/math/math_operator.rb
+++ src/math/math_operator.rb
@@ -1,92 +1,89 @@
 #! /usr/bin/ruby
+# frozen_string_literal: true
 
 N = 256
 
 ope = []
-ope_str = [];
-IO.foreach(ARGV[0]) { |l|
+ope_str = []
+IO.foreach(ARGV[0]) do |l|
   l.chomp!
-  next if (l.length < 1)
+  next if l.empty?
+
   ope.push(l[0])
   ope_str.push(l.split)
-}
+end
 
-ope_str = ope_str.uniq.sort {|a, b| b[0].length - a[0].length}
+ope_str = ope_str.uniq.sort { |a, b| b[0].length - a[0].length }
 
-OPE_PREFIX = "MATH_OPERATOR_TYPE"
+OPE_PREFIX = 'MATH_OPERATOR_TYPE'
 
-File.open("#{ARGV[1]}.h", "w") { |f|
-  f.puts <<EOF
-#ifndef MATH_SCANNER_OPE_HEADER
-#define MATH_SCANNER_OPE_HEADER
+File.open("#{ARGV[1]}.h", 'w') do |f|
+  f.puts <<~HEADER
+    #ifndef MATH_SCANNER_OPE_HEADER
+    #define MATH_SCANNER_OPE_HEADER
 
-enum MATH_OPERATOR_TYPE {
-EOF
-  ope_str.map {|s|
-    s[1]
-  }.uniq.sort.each { |s|
-    f.puts("  #{OPE_PREFIX}_#{s},")
-  }
+    enum MATH_OPERATOR_TYPE {
+  HEADER
+  ope_str.map { |s| s[1] }.uniq.sort.each { |s| f.puts("  #{OPE_PREFIX}_#{s},") }
   f.puts("  #{OPE_PREFIX}_UNKNOWN\n};\n\n")
 
-  f.puts <<EOF
-int math_scanner_is_ope(int chr);
-enum #{OPE_PREFIX} math_scanner_check_ope_str(const char *str, int *len);
+  f.puts <<~FOOTER
+    int math_scanner_is_ope(int chr);
+    enum #{OPE_PREFIX} math_scanner_check_ope_str(const char *str, int *len);
 
-#endif
-EOF
-}
+    #endif
+  FOOTER
+end
 
-File.open("#{ARGV[1]}.c", "w") { |f|
-  f.puts <<EOF
-#include "config.h"
+File.open("#{ARGV[1]}.c", 'w') do |f|
+  f.puts <<~HEADER
+    #include "config.h"
 
-#include <string.h>
-#include "math_operator.h"
+    #include <string.h>
+    #include "math_operator.h"
 
-struct ope_str {
-  char *ope;
-  int len;
-  enum #{OPE_PREFIX} type;
-};
+    struct ope_str {
+      char *ope;
+      int len;
+      enum #{OPE_PREFIX} type;
+    };
 
-static struct ope_str OpeStr[] = {
-EOF
-  ope_str.each {|s|
+    static struct ope_str OpeStr[] = {
+  HEADER
+  ope_str.each do |s|
     f.puts("  {\"#{s[0].gsub('\\', '\\\\\\')}\", #{s[0].length}, #{OPE_PREFIX}_#{s[1]}},")
-  }
+  end
   f.puts("};\n\n")
 
   f.puts("static char OpeChar[#{N}] = {")
-  N.times {|i|
-    c = sprintf("%c", i)
-    f.puts(ope.include?(c) ? "  1,  /* #{i.chr} */" : "  0,")
-  }
-  f.puts("};")
+  N.times do |i|
+    c = format('%c', i)
+    f.puts(ope.include?(c) ? "  1,  /* #{i.chr} */" : '  0,')
+  end
+  f.puts('};')
 
-  f.puts <<EOF
+  f.puts <<~FOOTER
+    int
+    math_scanner_is_ope(int chr)
+    {
+      if (chr < 0 || chr >= (int) (sizeof(OpeChar) / sizeof(*OpeChar)))
+        return 0;
 
-int
-math_scanner_is_ope(int chr)
-{
-  if (chr < 0 || chr > (int) (sizeof(OpeChar) / sizeof(*OpeChar)))
-    return 0;
-
-  return OpeChar[chr];
-}
-
-enum #{OPE_PREFIX}
-math_scanner_check_ope_str(const char *str, int *len) {
-  unsigned int i;
-
-  for (i = 0; i < sizeof(OpeStr) / sizeof(*OpeStr); i++) {
-    if (strncmp(str, OpeStr[i].ope, OpeStr[i].len) == 0) {
-      *len = OpeStr[i].len;
-      return OpeStr[i].type;
+      return OpeChar[chr];
     }
-  }
 
-  return #{OPE_PREFIX}_UNKNOWN;
-}
-EOF
-}
+    enum #{OPE_PREFIX}
+    math_scanner_check_ope_str(const char *str, int *len) {
+      unsigned int i;
+
+      for (i = 0; i < sizeof(OpeStr) / sizeof(*OpeStr); i++) {
+        if (strncmp(str, OpeStr[i].ope, OpeStr[i].len) == 0) {
+          *len = OpeStr[i].len;
+          return OpeStr[i].type;
+        }
+      }
+
+      return #{OPE_PREFIX}_UNKNOWN;
+    }
+  FOOTER
+end
